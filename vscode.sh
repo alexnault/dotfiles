fc8d11c7eba94e5c64ea155ba2b7901169a96e90
Alex Nault <nault.alex@gmail.com>
2021-03-23 10:44:22 -0400

    Remove vscode script
    Using built-in vscode sync instead

--- vscode.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-dir_name=${0:a:h}
-
-ln -sf "$dir_name/vscode/settings.json" ~/Library/Application\ Support/Code/User/settings.json
-ln -sf "$dir_name/vscode/keybindings.json" ~/Library/Application\ Support/Code/User/keybindings.json
